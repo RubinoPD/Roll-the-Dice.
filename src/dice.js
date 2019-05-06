eaaf74ff54d21f585140ddd88858cc697eeedabf
RubinoPD <robertaskask@gmail.com>
2019-05-06 12:57:47 +0300

    add dice component and styling

---
+++ src/dice.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Dice.css';
+
+class Dice extends Component {
+	render() {
+		return (
+			<div>
+				<i className={`Dice fas fa-dice-${this.props.face}`} />
+			</div>
+		);
+	}
+}
+
+export default Dice;
